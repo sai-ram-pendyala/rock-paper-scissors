ec3585847e9f20f880ab1b5778aca9bb845e2565
sai-ram-pendyala <89296568+sai-ram-pendyala@users.noreply.github.com>
2023-02-10 22:23:41 +0530

    Add playRound and game functions to js

--- javascript.js
+++ javascript.js
@@ -4,4 +4,31 @@ function getComputerChoice() {
     return choices[randomInd]
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection)
+        return "It's a tie!"
+    if(playerSelection == 'rock' && computerSelection == 'paper')
+        return "You Lose! Paper beats Rock"
+    if(playerSelection == 'rock' && computerSelection == 'scissors')
+        return "You Win! Rock beats Scissors"
+    if(playerSelection == 'paper' && computerSelection == 'scissors')
+        return "You Lose! Scissors beat Paper"
+    if(playerSelection == 'paper' && computerSelection == 'rock')
+        return "You Win! Paper beats Rock"
+    if(playerSelection == 'scissors' && computerSelection == 'rock')
+        return "You Lose! Rock beats Scissors"
+    if(playerSelection == 'scissors' && computerSelection == 'paper')
+        return "You Win! Scissors beat Paper"
+}
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Enter your choice")
+        const computerSelection = getComputerChoice()
+        console.log(playRound(playerSelection.toLocaleLowerCase(), computerSelection))
+    }
+}
+
+game()
+
+
